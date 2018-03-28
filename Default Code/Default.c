569a1b1ffa8fa6bea466584d31e513224226288a
Josh Simon <jsimon@students.rzjhs.org>
2018-03-27 21:19:36 -0500

    Finished programming line following

--- Default Code/Default.c
+++ Default Code/Default.c
@@ -20,8 +20,17 @@ bool robot = false;
 
 //Change these variables to the ports that the robot is in:
 
-//The port for the light that starts the robot:
+//The port for the light sensor that starts the robot:
 unsigned int startPort = 0;
+
+//The port for the right line sensor:
+unsigned int rLineSensorPort = 0;
+
+//The port for the left line sensor:
+unsigned int lLineSensorPort = 0;
+
+//The distance between the two line sensors:
+double lineSensorDist = 0.0;
 
 //If you are using the Lego robot these ports must also be set:
 
@@ -53,13 +62,42 @@ double PID_control(Error,pError,Integral,dt) {
     double d = kD*(Error-pError)/dt
     return p+i+d;
 }
+double whiteValue = 0;
+double blackValue = 0;
 void go_to_line() {
 }
 void follow_line(double Speed, double dt) {
   double pError = 0;
   double Integral = 0;
   while(/*Condition to continue following*/) {
-    double error = /*Get error from somewhere*/;
+    double lSense = analog(lLineSensorPort);
+    double rSense = analog(rLineSensorPort);
+    // simulates colors on a quadratic curve where the center of the robot is at 0
+    // ax^2+bx+c
+    // Finds the color sensors x-coordinate
+    // lSpot = -lineSensorDist/2;
+    // rSpot = lineSensorDist/2;
+    // a(lSpot)^2+b(lSpot)+c=lSense
+    // a(rSpot)^2+b(rSpot)+c=rSense
+    // a((rSpot)^2-(lSpot)^2)+b((rSpot)-(lSpot))=rSense-lSense
+    // d = lineSensorDist
+    // a(d/2)^2+b(d/2)-a(-d/2)^2-b(-d/2)=rSense-lSense
+    // a(d/2)^2-a(d/2)^2+bd/2+bd/2=rSense-lSense
+    // bd=rSense-lSense
+    // b=(rSense-lSense)/d
+    // a(d/2)^2+((rSense-lSense)/d)(d/2)=rSense
+    // a(d/2)^2+(rSense-lSense)/2=rSense
+    // a(d/2)^2=rSense/2+lSense/2
+    // a=((rSense+lSense)/2)*4/(d^2)
+    // a=2*(rSense+lSense)/(d^2)
+    // by taking the derivative, the extrema is -b/2a so:
+    // error = -b/2a
+    // error = -b/(4*(rSense+lSense)/(d^2))
+    // error = -b*(d^2)/(4*(rSense+lSense))
+    // error = -(rSense-lSense)*d/(4*(rSense+lSense))
+    // for this purpose, the calculation is easier if the error is positive. so:
+    double error = (rSense-lSense)*lineSensorDist/(4*(rSense+lSense));
+    lineSensorDist
     Integral += error*dt;
     double control = PID_control(error,pError,Integral,dt);
     pError = error;
