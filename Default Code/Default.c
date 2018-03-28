633cdf57b24d3214dcfc6d558d3543ce8b651771
Josh Simon <jsimon@students.rzjhs.org>
2018-03-28 09:07:26 -0500

    Fixed errors in line following.

--- Default Code/Default.c
+++ Default Code/Default.c
@@ -41,7 +41,7 @@ unsigned int lWheel = 0;
 unsigned int rWheel = 1;
 
 
-void move_at_power(double lSpeed, double rSpeed) {
+void move_at_power_n(double lSpeed, double rSpeed) {
   if(robot) {
     motor_power(lWheel,lSpeed);
     motor_power(rWheel,rSpeed);
@@ -62,6 +62,21 @@ double PID_control(Error,pError,Integral,dt) {
     double d = kD*(Error-pError)/dt
     return p+i+d;
 }
+void move_at_power(double lSpeed, double rSpeed, double time, double dt) {
+  double t = 0;
+  double pError = 0;
+  double Integral = 0;
+  while(t<time) {
+    double error = /*Get error*/;
+    Integral += error*dt;
+    double control = PID_control(error,pError,Integral,dt);
+    move_at_power_n(lSpeed-control, rSpeed+control);
+    msleep(1000.0*dt);
+    t += dt;
+    pError = error;
+  };
+  stop_moving();
+}
 double whiteValue = 0;
 double blackValue = 0;
 void go_to_line() {
@@ -72,37 +87,46 @@ void follow_line(double Speed, double dt) {
   while(/*Condition to continue following*/) {
     double lSense = analog(lLineSensorPort);
     double rSense = analog(rLineSensorPort);
-    // simulates colors on a quadratic curve where the center of the robot is at 0
-    // ax^2+bx+c
-    // Finds the color sensors x-coordinate
-    // lSpot = -lineSensorDist/2;
-    // rSpot = lineSensorDist/2;
-    // a(lSpot)^2+b(lSpot)+c=lSense
-    // a(rSpot)^2+b(rSpot)+c=rSense
-    // a((rSpot)^2-(lSpot)^2)+b((rSpot)-(lSpot))=rSense-lSense
     // d = lineSensorDist
-    // a(d/2)^2+b(d/2)-a(-d/2)^2-b(-d/2)=rSense-lSense
-    // a(d/2)^2-a(d/2)^2+bd/2+bd/2=rSense-lSense
-    // bd=rSense-lSense
-    // b=(rSense-lSense)/d
-    // a(d/2)^2+((rSense-lSense)/d)(d/2)=rSense
-    // a(d/2)^2+(rSense-lSense)/2=rSense
-    // a(d/2)^2=rSense/2+lSense/2
-    // a=((rSense+lSense)/2)*4/(d^2)
-    // a=2*(rSense+lSense)/(d^2)
-    // by taking the derivative, the extrema is -b/2a so:
-    // error = -b/2a
-    // error = -b/(4*(rSense+lSense)/(d^2))
-    // error = -b*(d^2)/(4*(rSense+lSense))
-    // error = -(rSense-lSense)*d/(4*(rSense+lSense))
-    // for this purpose, the calculation is easier if the error is positive. so:
-    double error = (rSense-lSense)*lineSensorDist/(4*(rSense+lSense));
-    lineSensorDist
+    // simulates colors on a quadratic curve where the center of the robot is at 0
+    // a(x-m)^2+c
+    // c = blackValue
+    // a(d/2-m)^2+blackValue=rSense
+    // a(d/2-m)^2 = rSense - blackValue
+    // a = (rSense - blackValue)/(d/2-m)^2
+    // 
+    // a(d/2+m)^2+blackValue=lSense
+    // a(d/2+m)^2 = lSense - blackValue
+    // a = (lSense - blackValue)/(d/2+m)^2
+    // (rSense - blackValue)/(d/2-m)^2 = (lSense - blackValue)/(d/2+m)^2
+    // (rSense - blackValue)*(d/2+m)^2 = (lSense - blackValue)*(d/2-m)^2
+    // (rSense - blackValue)*(d^2/4+d*m+m^2) = (lSense - blackValue)*(d^2/4-d*m+m^2)
+    // (rSense - blackValue)*d^2/4+(rSense - blackValue)*d*m+(rSense - blackValue)*m^2 = (lSense - blackValue)*d^2/4-(lSense - blackValue)*d*m+(lSense - blackValue)*m^2
+    // (rSense - blackValue)*d^2/4+(rSense - blackValue)*d*m+(lSense - blackValue)*d*m+(rSense - blackValue)*m^2 = (lSense - blackValue)*d^2/4+(lSense - blackValue)*m^2
+    // (rSense - blackValue)*d^2/4+d*m*((rSense - blackValue)+(lSense - blackValue))+(rSense - blackValue)*m^2 = (lSense - blackValue)*d^2/4+(lSense - blackValue)*m^2
+    // (rSense - blackValue)*d^2/4+d*(rSense + lSense - 2 * blackValue)*m+(rSense - blackValue)*m^2 = (lSense - blackValue)*d^2/4+(lSense - blackValue)*m^2
+    // (d^2/4)*((rSense - blackValue)-(lSense - blackValue))+d*(rSense + lSense - 2 * blackValue)*m+(rSense - blackValue)*m^2 = (lSense - blackValue)*m^2
+    // (d^2/4)*(rSense - lSense)+d*(rSense + lSense - 2 * blackValue)*m+(rSense - blackValue)*m^2 = (lSense - blackValue)*m^2
+    // (d^2/4)*(rSense - lSense)+d*(rSense + lSense - 2 * blackValue)*m+m^2*((rSense - blackValue) - (lSense - blackValue)) = 0
+    // (d^2/4)*(rSense - lSense)+d*(rSense + lSense - 2 * blackValue)*m+m^2*(rSense - lSense) = 0
+    // m = -d*(rSense + lSense - 2 * blackValue)/(2*(rSense - lSense))
+    // The error is -m
+    double mult = 1;
+    if(whiteValue < blackValue) {
+      double mult = -1;
+    };
+    if(mult * blackValue > mult * lSense) {
+      blackValue = lSense;
+    };
+    if(mult * blackValue > mult * rSense) {
+      blackValue = rSense;
+    };
+    double error = d*(rSense + lSense - 2.0 * blackValue)/(2.0 * (rSense - lSense));
     Integral += error*dt;
     double control = PID_control(error,pError,Integral,dt);
     pError = error;
-    move_at_power(Speed-control,Speed+control);
-    msleep(timeStep);
+    move_at_power_n(Speed-control,Speed+control);
+    msleep(1000.0*dt);
   };
 }
 void code() {
