6376d7027db8967faa134164001ac69eefc681fb
Eyal Noy <Orangelover1200@gmail.com>
2019-03-26 17:03:24 -0500

    Update Main.c

--- Music Testing/Main.c
+++ Music Testing/Main.c
@@ -11,30 +11,12 @@ typedef enum { false, true } bool;
 double pos[] = {0,0,0};
 
 int gc_song_array[16][33] = {
-    //Serial sequence: [140] [Song Number] [Song Length] [Note Number 1] [Note Duration 1] [Note Number 2] [Note Duration 2], etc. 
-    {16, 294}
+    //Array sequence: [Song Length] [Note Number 1] [Note Duration 1] [Note Number 2] [Note Duration 2], etc. 
+    {11, 62, 8, 62, 8, 74, 16, 69, 24, 68, 8, 0, 8, 67, 16, 65, 16, 62, 8, 65, 8, 67, 8},
 }
 
 void birthday() {
     create_connect();
-    int gc_song_array[16][33];
-    gc_song_array[0][0]=16;
-    gc_song_array[0][1]=0;
-    gc_song_array[0][2]=1;
-    gc_song_array[0][3]=32;
-    gc_song_array[0][4]=1;
-    gc_song_array[0][5]=0;
-    gc_song_array[0][6]=1;
-    gc_song_array[0][7]=60;
-    gc_song_array[0][8]=22;
-    gc_song_array[0][9]=60;
-    gc_song_array[0][10]=7;
-    gc_song_array[0][11]=62;
-    gc_song_array[0][12]=29;
-    gc_song_array[0][13]=60;
-    gc_song_array[0][14]=29;
-    gc_song_array[0][15]=53;
-    gc_song_array[0][16]=26;
     create_load_song(0);
     while(get_create_song_playing(.01)) {msleep(50);}
 }
