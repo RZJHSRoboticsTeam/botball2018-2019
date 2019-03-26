c6514c0368d2c597bd9f68ddee6ded8569711e8b
Eyal Noy <Orangelover1200@gmail.com>
2019-03-26 17:09:26 -0500

    Music added

--- Music Testing/Main.c
+++ Music Testing/Main.c
@@ -13,7 +13,7 @@ double pos[] = {0,0,0};
 int gc_song_array[16][33] = {
     //Array sequence: [Song Length] [Note Number 1] [Note Duration 1] [Note Number 2] [Note Duration 2], etc. 
     {11, 62, 8, 62, 8, 74, 16, 69, 24, 68, 8, 0, 8, 67, 16, 65, 16, 62, 8, 65, 8, 67, 8},
-}
+};
 
 void birthday() {
     create_connect();
