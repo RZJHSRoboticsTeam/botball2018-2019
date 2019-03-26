d6dc3688fd449ff3a1e0d265b09667cc2e2eaaf9
Eyal Noy <Orangelover1200@gmail.com>
2019-03-26 17:11:14 -0500

    Update Main.c

--- Music Testing/Main.c
+++ Music Testing/Main.c
@@ -8,11 +8,10 @@
 #include <kipr/botball.h>
 #include <math.h>
 typedef enum { false, true } bool;
-double pos[] = {0,0,0};
 
 int gc_song_array[16][33] = {
     //Array sequence: [Song Length] [Note Number 1] [Note Duration 1] [Note Number 2] [Note Duration 2], etc. 
-    {11, 62, 8, 62, 8, 74, 16, 69, 24, 68, 8, 0, 8, 67, 16, 65, 16, 62, 8, 65, 8, 67, 8},
+    {11, 62, 8, 62, 8, 74, 16, 69, 24, 68, 8, 0, 8, 67, 16, 65, 16, 62, 8, 65, 8, 67, 8}
 };
 
 void birthday() {
